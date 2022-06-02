3b42475574ee2e729854226437dc6b562fa6ada9
Good Lookin <r_a_msmlm@outlook.com>
2022-06-02 13:33:03 +0300

    Passowrd Hashing

--- api/users/users.controllers.js
+++ api/users/users.controllers.js
@@ -1,26 +1,31 @@
-const User = require('../../models/User');
+const User = require("../../models/User");
+const bcrypt = require("bcrypt");
 
 exports.signin = async (req, res) => {
   try {
   } catch (err) {
-    res.status(500).json('Server Error');
+    res.status(500).json("Server Error");
   }
 };
 
 exports.signup = async (req, res) => {
+  const { password } = req.body;
+  const salts = 10;
   try {
+    const hashedPass = await bcrypt.hash(password, salts);
+    req.body.password = hashedPass;
     const newUser = await User.create(req.body);
     res.status(201).json(newUser);
   } catch (err) {
-    res.status(500).json('Server Error');
+    res.status(500).json("Server Error");
   }
 };
 
 exports.getUsers = async (req, res) => {
   try {
-    const users = await User.find().populate('urls');
+    const users = await User.find().populate("urls");
     res.status(201).json(users);
   } catch (err) {
-    res.status(500).json('Server Error');
+    res.status(500).json("Server Error");
   }
 };
--- database.js
+++ database.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect('Your connection string', {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
+  const conn = await mongoose.connect(
+    "mongodb+srv://Password:Password@cluster0.m1vnr.mongodb.net/test",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  );
 
   console.log(`mongo connected: ${conn.connection.host}`);
 };
